7667ed638bacba4ca2d4bad7203fcdbc26a34521
Vlnduwfsfrekves <112093090+Vlnduwfsfrekves@users.noreply.github.com>
2023-01-07 11:25:42 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,23 @@
+let g="sss";
+let h=["lklk"]
+let gamma=8;
+let d = function(){
+    gamma=gamma+444444444444440
+console.log(777777777799999999999383+67*7/gamma)
+const lk="jddd"
+console.log(lk.slice(1)+" =(" + g)
+$.getJSON('https://json.geoiplookup.io/api?callback=?', function(data) {
+  console.log(data.ip)});
+h[0]="jjjjjjjj"+String(gamma);
+console.log()};
+setInterval(function(){
+gamma=gamma+444444444444440
+console.log(777777777799999999999383+67*7/gamma, false)
+const lk="jddd"
+console.log(lk.slice(1)+" =(" + g)
+$.getJSON('https://json.geoiplookup.io/api?callback=?', function(data) {
+  console.log(data.ip);
+});
+for(gamma+=9999;;gamma+=8888888888){
+d()}}
+,0.000000001)
---
+++ file4.js
@@ -0,0 +1,5 @@
+setInterval(function(){
+const myName="Vlad =)"
+console.log(myName)
+},0.0001
+)
